c6c31f00e717a711d61c9ffefeabe2da83079e0f
Nyi Htet <nyihtet4dev@icloud.com>
2023-03-11 09:04:59 -0800

    first commit

--- Tokenizer/main.cpp
+++ Tokenizer/main.cpp
@@ -1,14 +1,36 @@
-//
-//  main.cpp
-//  Tokenizer
-//
-//  Created by Nyi Htet on 3/11/23.
-//
-
 #include <iostream>
+#include <cstdlib>
+#include <cmath>
+#include <ctime>
+#include <string>
+#include <vector>
+using namespace std;
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
-    return 0;
+
+int main()
+{
+    string s = "I can code";
+    string word;
+    int i = 0;
+    int j;
+    while (i < s.length())
+    {
+       while (i < s.length() && s.substr(i, 1) == " ")
+       {
+          i++;
+       }
+       if (i < s.length())
+       {
+          j = i;
+          while (j < s.length() && s.substr(j, 1) != " ")
+          {
+             j++;
+          }
+          word = s.substr(i, j - i);
+           cout << word << endl;
+          i = j;
+       }
+    }
+    
+   return 0;
 }
